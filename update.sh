bcaa336e0b8297b983324b3c48580dc3871e7db7
root <root@raspberrypi.(none)>
2015-03-28 13:57:47 -0800

    IP Address update

--- update.sh
+++ update.sh
@@ -1,6 +1,4 @@
 cd /home/pi/RaspberryPi/
-rm -r IPAddress.txt
-echo "This is the output of the ifconfig command:" >> IPAddress.txt
 ifconfig >> IPAddress.txt
 git add --all
 git commit -m "IP Address update"
